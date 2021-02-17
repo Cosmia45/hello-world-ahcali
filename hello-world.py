4b4d32c9143384037817c7409f9a1269730a869b
Liam Juskevice <LiamJuskevice42@Gmail.com>
2021-02-17 13:14:42 -0500

    initialized the file
    yep.

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+"""
+Our first Python test program.
+Team members: Ahmed Hameed, Camilo Pareja, Liam Juskevice
+"""
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
